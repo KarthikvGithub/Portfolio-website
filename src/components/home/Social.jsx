1423f55fa248d35f915997f503478e3455c806cd
KarthikvGithub <valdaskarthikgoud@gmail.com>
2024-06-13 23:10:17 +0530

    Almost..!!

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/karthikvaldas_06/" className="home__social-icon" target='_blank' rel="noreferrer">
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/karthik-valdas-220591241/" className="home__social-icon" target='_blank' rel="noreferrer">
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/KarthikvGithub" className="home__social-icon" target='_blank' rel="noreferrer">
+            <i class="uil uil-github"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social;
