94560159369683a4796109a300eea8d701535854
KarthikvGithub <valdaskarthikgoud@gmail.com>
2024-06-15 15:41:33 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/header/Header.jsx';
-import Home from './components/home/Home.jsx';
-import About from './components/about/About.jsx';
-import Skills from './components/skills/Skills.jsx';
-import Services from './components/services/Services.jsx';
-import Qualification from './components/qualification/Qualification.jsx';
-import Work from './components/work/Work.jsx';
-import Contact from './components/contact/Contact.jsx';
-import Footer from './components/footer/Footer.jsx';
-import ScrollUp from './components/scrollup/ScrollUp.jsx';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Work from './components/work/Work';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
 
 function App() {
   const [activeNav, setActiveNav] = useState('');
