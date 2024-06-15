b5c3a13746900a929ccf1f261b60eb0a955b1229
KarthikvGithub <valdaskarthikgoud@gmail.com>
2024-06-15 15:47:12 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
